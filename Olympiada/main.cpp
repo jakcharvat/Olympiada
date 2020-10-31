38076341c98e23ca28a7cc13f7fc8555cbc499b7
Jakub Charvat <jakcharvat@gmail.com>
2020-10-31 17:02:54 +0100

    Initial Commit

---
+++ Olympiada/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Olympiada
+//
+//  Created by Jakub Charvat on 31.10.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
